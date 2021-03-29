9e9efb582e826f152068fa9d378ad27c8119cddd
Neta Globerman <neta.globerman@gmail.com>
2021-03-29 09:13:27 +0300

    chore(app:settings)

--- src/main/kotlin/reviewbot/infra/Settings.kt
+++ src/main/kotlin/reviewbot/infra/Settings.kt
@@ -1,4 +1,11 @@
+@file:Suppress("PropertyName")
+
 package reviewbot.infra
+
+import com.typesafe.config.Config
+import com.typesafe.config.ConfigFactory
+import io.github.config4k.extract
+import reviewbot.db.MongoConfig
 
 data class BotConfig(
     val id: String,
@@ -7,3 +14,8 @@ data class BotConfig(
     val webhookVerifyMode: String,
     val baseUrl: String,
 )
+
+class Settings(config: Config = ConfigFactory.load()) {
+    val BotConfig = config.extract<BotConfig>("bot")
+    val MongoConfig = config.extract<MongoConfig>("mongo")
+}

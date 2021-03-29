6d0356a4852308a95a0cf0bc8c7a953f71b03bbd
Neta Globerman <neta.globerman@gmail.com>
2021-03-29 08:57:09 +0300

    chore(kotlin:lint)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.21"
+    id("com.diffplug.spotless") version "5.11.1"
     application
 }
 
@@ -39,7 +40,15 @@ tasks {
         kotlinOptions.freeCompilerArgs = listOf("-Xinline-classes", "-Xopt-in=kotlin.RequiresOptIn")
     }
 }
+configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+    kotlin {
+        ktlint("0.39.0").userData(mapOf("disabled_rules" to "no-wildcard-imports,filename"))
+    }
 
+    kotlinGradle {
+        target("*.gradle.kts")
+        ktlint("0.39.0")
+    }
 }
 
 application {

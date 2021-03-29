60c72bd2dced8482c60c3611ac0d1db772ac9f80
Neta Globerman <neta.globerman@gmail.com>
2021-03-29 09:30:40 +0300

    fix(ktor:app)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,29 +1,50 @@
-import io.ktor.application.call
-import io.ktor.html.respondHtml
-import io.ktor.http.HttpStatusCode
-import io.ktor.routing.get
-import io.ktor.routing.routing
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.netty.Netty
-import kotlinx.html.*
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.serialization.*
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.put
+import org.koin.ktor.ext.Koin
+import org.slf4j.event.Level
+import reviewbot.infra.koinModules
+import reviewbot.routes.pages
+import reviewbot.routes.reviews
+import reviewbot.routes.triggers
+import reviewbot.routes.webhooks
 
-fun HTML.index() {
-    head {
-        title("Hello from Ktor!")
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+fun Application.module() {
+    install(CallLogging) {
+        level = Level.INFO
     }
-    body {
-        div {
-            +"Hello from Ktor"
+
+    install(Koin) {
+        modules(koinModules)
+    }
+
+    install(ContentNegotiation) { json(reviewbot.infra.json) }
+
+    install(StatusPages) {
+        exception<SerializationException> { cause ->
+            log.error("failed parsing request payload", cause)
+            call.respond(HttpStatusCode.BadRequest)
+        }
+
+        exception<RuntimeException> { cause ->
+            log.error("unhandled exception while handling call", cause)
+            call.respond(HttpStatusCode.InternalServerError)
         }
     }
-}
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "127.0.0.1") {
-        routing {
-            get("/") {
-                call.respondHtml(HttpStatusCode.OK, HTML::index)
-            }
-        }
-    }.start(wait = true)
+    routing {
+        get("/ping") { call.respond(HttpStatusCode.OK, buildJsonObject { put("status", "pong") }) }
+        triggers()
+        webhooks()
+        pages()
+        reviews()
+    }
 }
